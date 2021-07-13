dcfb0503f194557af16d531e8e2fd154bc26c7ea
Vanessa <vanessa.chodaton@thalesgroup.com>
2021-07-13 15:14:49 +0200

    init Project

---
+++ src/main/java/com/total/smartcomp/SmartcompApplication.java
@@ -0,0 +1,13 @@
+package com.total.smartcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/total/smartcomp/SmartcompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.total.smartcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
